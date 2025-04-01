8172fe229a9c14414c487ba2aef100a55eae1dce
Penrose0v0 <huyang0904@outlook.com>
2025-04-01 22:18:26 +0900

    non-local deepseek

--- agent_project/src/llms.py
+++ agent_project/src/llms.py
@@ -2,7 +2,7 @@ from openai import OpenAI
 import ollama
 import json
 
-from .utils import logger
+from utils import logger
 
 
 class LLMBase: 
@@ -58,21 +58,41 @@ class DeepSeek(LLMBase):
     def __init__(self, **kwargs): 
         super().__init__()
         self.system_prompt[0]["role"] = "system"
-        self.send_to_model = lambda messages: ollama.chat(
-            model="deepseek-r1:14b", 
+        self.local = kwargs["local"]
+        if kwargs["local"]:
+            self.send_to_model = lambda messages: ollama.chat(
+                model="deepseek-r1:14b", 
+                messages=messages, 
+                options={
+                    "temperature": kwargs["temperature"], 
+                    "top_p": kwargs["top_p"]
+                }
+            )
+
+        else:
+            base_url = "https://api.siliconflow.cn/v1"
+            api_key_path = kwargs["api_key_path"]
+            with open(api_key_path, 'r', encoding='utf-8') as file: 
+                api_key = file.read().strip()
+            client = OpenAI(api_key=api_key, base_url=base_url)
+
+            self.send_to_model = lambda messages: client.chat.completions.create(
+            model="Pro/deepseek-ai/DeepSeek-V3",
             messages=messages, 
-            options={
-                "temperature": kwargs["temperature"], 
-                "top_p": kwargs["top_p"]
-            }
+            temperature=kwargs["temperature"],
+            top_p=kwargs["top_p"]
         )
 
     def response(self, prompt, check_response=None): 
         self.memory.append({"role": "user", "content": prompt})
         messages = self.system_prompt + self.memory
         completion = self.send_to_model(messages)
-        think, response = map(lambda x: x.strip(), completion.message.content.split("</think>"))
-        logger.debug(f"Think: {think}")
+        content = completion.message.content if self.local else completion.choices[0].message.content
+        if "</think>" in content:
+            think, response = map(lambda x: x.strip(), content.split("</think>"))
+            logger.debug(f"Think: {think}")
+        else:
+            response = content
 
         if check_response is None or check_response(response): 
             self.memory.append({"role": "assistant", "content": response})
@@ -98,8 +118,11 @@ class QWQ(LLMBase):
         self.memory.append({"role": "user", "content": prompt})
         messages = self.system_prompt + self.memory
         completion = self.send_to_model(messages)
-        think, response = map(lambda x: x.strip(), completion.message.content.split("</think>"))
-        logger.debug(f"Think: {think}")
+        if "</think>" in completion.message.content:
+            think, response = map(lambda x: x.strip(), completion.message.content.split("</think>"))
+            logger.debug(f"Think: {think}")
+        else:
+            response = completion.message.content
 
         if check_response is None or check_response(response): 
             self.memory.append({"role": "assistant", "content": response})
@@ -119,11 +142,13 @@ def make_llm(config: dict) -> LLMBase:
     else: 
         raise ValueError(f"Unknown LLM: {config['llm']}")
     
+    local = config["local"] if "local" in config else False
     api_key_path = config["api_key_path"] if "api_key_path" in config else None
     temperature = config["temperature"] if "temperature" in config else None
     top_p = config["top_p"] if "top_p" in config else None
 
     return llm_cls(
+        local=local,
         api_key_path=api_key_path, 
         temperature=temperature, 
         top_p=top_p
